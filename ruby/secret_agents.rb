e2acf9f39a8b976a2e795a8943d2e40879411839
Catherine Rice <clr2107@gmail.com>
2017-07-19 15:10:26 -0400

    Add driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,26 +1,56 @@
-#Pseudocode - Encrypt Method
-#For each letter in the string, advance one letter forward and return the output (add one to the index).
+#Encrypt (with edge case)
+#For each letter in the string, go forward one letter in the alphabet (add one to the index).
 def encrypt(string)
-new_string=""
-idx=0
+	new_string=""
+	idx=0
 while idx < string.length
-new_string << string[idx].next
-idx+=1
+	new_string << string[idx].next unless
+	if string[idx] == "z"
+		string[idx] = "a"
+		new_string << string[idx]
 end
-new_string
+	idx+=1
 end
-encrypt(string)
+	new_string
+end
+#encrypt(string)
+#encrypt("abc")
+#encrypt("zed")
 
-#Pseudocode - Decrypt
+#Decrypt 
 #For each letter in the string, go back in the alphabet one letter (subtract one to the index).
 def decrypt(string)
   idx = 0
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   while idx < string.length
-  string[idx] = alphabet[alphabet.index(string[idx]) - 1]
-  idx += 1
+  	string[idx] = alphabet[alphabet.index(string[idx]) - 1]
+  	idx += 1
 end
-string
+	string
 end
-decrypt(string)
+#decrypt(string)
+#decrypt("bcd")
+#decrypt("afe")
 
+#NESTED METHOD
+#decrypt(encrypt("swordfish"))
+#This nested method works because first the string is being encrypted, and then it is being decrypted.
+#If something was decrypted before being encrypted, it would defeat the purpose of encryption and be meaningless.
+
+#DRIVER CODE 
+#Asks user if they would like to encrypt or decrypt a password, stores this as input.
+#Include error message if input is outside scope of the question.
+#Asks user for password (string) and runs encrypt/decrypt methods according to the user's selection.
+	puts "Hello. Would you like to decrypt or encrypt a password? "
+	input = gets.chomp
+until input == "encrypt" || input == "decrypt"
+	puts "Please try again."
+	input = gets.chomp
+end
+	puts "Please enter your password."
+	password = gets.chomp
+if input == "encrypt"
+	puts encrypt(password)
+elsif input == "decrypt"
+	puts decrypt(password)
+end
