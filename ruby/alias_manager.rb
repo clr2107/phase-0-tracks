f1d93b440b25f6a9c6ae4dd277ea37569e314b5c
Catherine Rice <clr2107@gmail.com>
2017-08-08 00:38:33 -0400

    Reorganize methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,6 @@
-#vowel function
+#Alias manager takes a name and gives back a fake name. The fake name has a swapped first and last name and changes all vowels to the next vowel in 'aeiou' and all of the consonants to the next consonant.
+
+#Next vowel method (to be invoked in alias_manager method.) Changes each vowel to next vowel in 'aeiou'.
 def next_vowel(letter)
     vowels = ["a", "e", "i", "o", "u"]
     #edge case
@@ -9,9 +11,10 @@ def next_vowel(letter)
     return vowels[next_vowel_index]
 end
 
-#consonant function
+#Next consonant method (to be invoked in alias_manager method.) Changes each consonant to next consonant in alphabet.
 def next_consonant(letter)
     consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+    #edge case
     if letter == "z"
       return "b"
     end
@@ -19,7 +22,10 @@ def next_consonant(letter)
     return consonants[next_consonant_index]
 end
 
-#convert name to next vowel/consonant function
+#Swap order of first and last name (to be invoked in alias_manager method.)
+
+
+#Main method: Convert name to alias. Using .each, check if each letter in name (user input) is a vowel or a consonant (using .include? method), invoke next_vowel or next_consonant depending on letter is a vowel or consonant, and save new letters in a variable "new_name". Account for spaces in name with " ". Invoke swap_name method at the end to swap order of first and last name and return the result.
 def alias_manager(name)
 vowels = ["a", "e", "i", "o", "u"]
 consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
@@ -36,27 +42,20 @@ new_name = ""
      new_name << " "
     end
   end
-  swap_name(new_name, name)
+  swap_name(new_name)
 end
 
-#swap order of first and last names; save and print all aliases.
-def swap_name(new_name, name)
-  name = name.join("").capitalize
-all_names = []
-new_name =  new_name.split(" ")
-new_name[0], new_name[1] = new_name[1], new_name[0]
-puts final_name = new_name[0].capitalize + " " + new_name[1].capitalize
-all_names << final_name
-all_names.each do |x|
-  puts "#{name} is actually #{final_name}."
-end
-end
-
-#ask for user input
+#DRIVER CODE
+#Ask for user input, store all names (original and fake names) in a hash after running alias_manager method and print all names.
 puts "Enter your name for an alias. After all names have been entered, type done."
-name = gets.chomp
-until name == "done"
-alias_manager(name)
-name = gets.chomp
+all_names = {}
+loop do
+  name = gets.chomp
+  break if name == "done"
+  all_names[name] = alias_manager(name)
 end
+all_names.each do |key,value|
+  puts "#{key} is actually #{value}."
+end
+
 
