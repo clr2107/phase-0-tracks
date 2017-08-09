08a6715ee46e4d3bb9a6c068d2251926e0aac59a
Catherine Rice <clr2107@gmail.com>
2017-08-08 20:42:26 -0400

    updated word game

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,69 +1,28 @@
-#game class
-class Wordgame
-end
-
-#initialize new instance of game
-def initialize
-  word = "" #word is empty string
-  @guess_count = 0
-  @is_over = false
-end
-
-def user_guess(letter, user_word)
-  @guess_count += 1
-  if word.include?(letter)
-    word #update word with correct letter
-  end
-  if @guess_count == word.length
-    @is_over = true
-    puts "You are out of guesses."
-  end
-  if word == user_word
-    @is_over == true
-    puts "You won!"
-end
-
-#User interface
-
-puts "Welcome to the word game! Try to guess my word."
-game = Wordgame.new
-
-while !game.is_over
-puts "Guess a letter."
-letter = gets.chomp
-
-
-##
 word = "cat"
-user_word = []
-index = 0
-word = word.split("")
-until word == user_word
-  length = word.length
+user_guess = " " #word.length.times do|x| puts "_" or something
+counter = 0
+correct_answer = false
+word_array = word.split("")
+until word == user_guess
   puts "What letter would you like to guess?"
   letter_guess = gets.chomp
-  user_word = "_"
-    # if word.include?(letter) #letter is "a"
-    #   correct_guess = true
-    #   letter_index = word.index(letter)
-    #   user_word[letter_index] = letter
-
-      # user_word[index] = "_"
-
-      word.each do |x|
-        if x == letter_guess
-          letter_index = word.index(letter_guess)
-          user_word[letter_index] = letter_guess
-          puts "Your guess is " + user_word
-        else
-
-
+  idx = 0
+    word_array.each do |letter_word|
+        if letter_word == letter_guess
+          letter_index = word.index(letter_word)
+          user_guess[letter_index] = letter_word
+        end
+        if letter_word != letter_guess
+        puts "Try again!" #if user guesses wrong letter
+        end
+    idx +=1
     end
-
-    end
-  # correct_guess = false
-  # puts "Try again loser."
-  # end
+    counter +=1
+    puts "Your current guess is " + user_guess
+    break if
+    counter == word.length*2 #user loses if # of guesses == length of word * 2
 end
-
-
+correct_answer = true
+      if correct_answer
+        puts "You guessed the word! YOU WON!!!"
+      end
