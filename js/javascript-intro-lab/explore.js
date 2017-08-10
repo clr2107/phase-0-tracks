df07ff6e0cef2828773bd22fcdf0a23a35bb441e
Catherine Rice <clr2107@gmail.com>
2017-08-10 17:49:38 -0400

    Create reverse method in JS

--- js/javascript-intro-lab/explore.js
+++ js/javascript-intro-lab/explore.js
@@ -1,13 +1,10 @@
 //Write a function that takes a string as a parameter and reverses the string.
 //Take the last letter of string and make it the first letter of new string. Take the second to last letter of string and make it the second letter of new string, etc. Add/concatenate the letters together and print to the console.
-//If string = "cat", string.length = 3, so use a for loop: var i = string.length - 1,
 
 function reverse(string) {
-
-  for (var i = string.length - 1; i >= 0; i-=1) {
-    var new_word = " "
-
-}
-
+  var new_string = "";
+  for (var i = string.length -1; i >= 0; i --)
+  new_string += string[i];
+  return new_string;
 }
 reverse("cat")
