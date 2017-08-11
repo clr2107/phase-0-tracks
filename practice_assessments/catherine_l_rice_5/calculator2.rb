cc88ac86b3cd0ed4a8b47e2c2aef372648faa9c3
Catherine Rice <clr2107@gmail.com>
2017-08-11 11:41:15 -0400

    Finalize calculate method

--- practice_assessments/catherine_l_rice_5/calculator2.rb
+++ practice_assessments/catherine_l_rice_5/calculator2.rb
@@ -1,4 +1,4 @@
-#Calculate method performs calculation based on user input.
+#The calculate method performs specific calculation based on user input.
 def calculate(integer_one, operator_string, integer_two)
   if operator_string == "+"
     answer = integer_one + integer_two
@@ -9,6 +9,7 @@ def calculate(integer_one, operator_string, integer_two)
   elsif operator_string == "/"
     answer = integer_one / integer_two
   end
+  puts answer
   return answer
 end
 # calculate(4, "+", 5)
@@ -16,27 +17,22 @@ end
 # calculate(4, "*", 5)
 # calculate(20, "/", 5)
 
-#User interface: Ask for user input (a calculation to perform). Use your method to give the user a result. If the user type "4 + 5" the program should print a result of 9 and then exit.
-#Update program to allow user to do as may calculations as they want. When the user types 'done' the program can exit.
-#When the user has finished performing calculations but just before the program exits, print a count of the calculations performed and a history of all of the calculations. (Using a hash).
-puts "What calculation would you like to perform?"
-input = gets.chomp
-until input == "done"
-  input = input.split(" ")
-  integer_one = input[0].to_i
-  operator_string = input[1]
-  integer_two = input[2].to_i
-  puts calculate(integer_one, operator_string, integer_two)
-  input = gets.chomp
-end
+#User interface: Ask for user input (a calculation to perform). Method will give the user the result of the calculation.
+#Updated program to allow user to do as may calculations as they want. When the user types 'done' the program exits.
 
-  all = {}
+all = {}
   loop do
-  input = gets.chomp
-  break if input == "done"
-  all[input] = calculate(integer_one, operator_string, integer_two)
-end
-  puts "#{all.length + 1} operations have been performed:"
+    puts "What calculation would you like to perform?"
+    input = gets.chomp
+    if input == "done"
+      break
+    end
+  input_array = input.split(" ")
+  all[input] = calculate(input_array[0].to_i, input_array[1], input_array[2].to_i)
+  end
+
+#When the user is finished entering calculations but before program exits, a count of the calculations performed and a history of all of the calculations is printed using data stored in a hash.
+puts "#{all.length} operations have been performed:"
   all.each do |key, value|
     puts "#{key} = #{value}"
   end
