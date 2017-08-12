81fcb9a68b7d3cc29a4a4a5ea1127655b94ce8f7
Catherine Rice <clr2107@gmail.com>
2017-08-12 14:09:21 -0400

    Add driver code

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+//Release 0: Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
 //Use each loop to find the length of each element in the array.
 //Sort the lengths from shortest to longest.
 //Return the last element (longest) in the array.
@@ -12,9 +12,11 @@ return (arr[arr.length -1])
 longestPhrase(['cat', 'elephant', 'dolphin']);
 longestPhrase(['giraffe', 'dog', 'bird'])
 
-//Write a function that takes two objects and checks to see if the objects share at least one key-value pair (return a boolean). Doesn't matter if a property is a string or identifier name.
-
-//
+//Release 1: Write a function that takes two objects and checks to see if the objects share at least one key-value pair (return a boolean). Doesn't matter if a property is a string or identifier name.
 function share_value(object1, object2) {
 
 }
+
+//Release 2: Write a function that takes an integer for length, and builds and returns an array of strings of the given length.
+//(Argument of 3 returns 3 randomly generated strings of varying length, min of 1 letter and max of 10 letters.)
+//Driver code: do 10 times: generates an array, prints the array,feeds array into longest word function, and prints result.
