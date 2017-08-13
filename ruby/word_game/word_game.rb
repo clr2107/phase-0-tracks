25637c2a8c42a940638dc23f09f1c83d4d899706
Catherine Rice <clr2107@gmail.com>
2017-08-12 23:50:34 -0400

    Update driver code and instance methods

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,6 +1,6 @@
 class WordGame
 
-attr_accessor :user_guess, :all_guesses, :game_over, :counter, :word
+attr_accessor :user_guess, :all_guesses, :game_over, :counter, :word, :letter_guess
 
 def initialize
   puts "Starting a new game..."
@@ -8,29 +8,33 @@ def initialize
   @counter = 0
   @user_guess = []
   @all_guesses = []
-
 end
 
 def create_user_guess(word)
-word.length.times do |x|
-  @user_guess << " _ "
+  word.length.times do |x|
+    @user_guess << " _ "
 end
 puts @user_guess.join("")
+end
+
+def check_duplicate(letter_guess)
+if @all_guesses.include?(letter_guess)
+  puts "You already guessed that letter."
+  end
 end
 
 #Method to get user input (letter), check if letter is correct/incorrect, and print the current word.
 def guess(word, letter_guess)
   word_array = word.split("")
-  word_array.each do |letter_word|
-    if letter_word == letter_guess
-      p @all_guesses << letter_guess
-      correct_letter = true
-      index = word.index(letter_guess)
-      @user_guess[index] = letter_guess
-      puts "You guessed a correct letter!"
-      puts "Your current guess is " + @user_guess.join("")
-      @counter+= 1
-    end
+  word_array.each_with_index do |letter_word, index|
+  if letter_word == letter_guess
+    p @all_guesses << letter_guess
+    correct_letter = true
+    @user_guess[index] = letter_guess
+    puts "You guessed a correct letter!"
+    puts "Your current guess is " + @user_guess.join("")
+    @counter+= 1
+  end
   end
   if !word.include?(letter_guess)
     p @all_guesses << letter_guess
@@ -43,7 +47,7 @@ def guess(word, letter_guess)
     @game_over = true
     puts "You won in #{@counter} guesses!"
   end
-  if @counter >= word.length * 2
+  if @counter > word.length * 2
     @game_over = true
     puts "You are out of guesses! Game over."
   end
@@ -55,16 +59,15 @@ end
 # DRIVER CODE
 puts "Welcome to the Word Game!"
 game = WordGame.new
-puts "User 1 enters a word."
+puts "User 1: Please enter a secret word."
 word = gets.chomp
 game.create_user_guess(word)
-
-
 while !game.game_over
-puts "User 2 guesses a letter."
-break if @game_over == true
-letter_guess = gets.chomp.to_s
-game.guess(word, letter_guess)
+  puts "User 2: Please guess a letter."
+  break if @game_over == true
+  letter_guess = gets.chomp.to_s
+  game.check_duplicate(letter_guess)
+  game.guess(word, letter_guess)
 end
 
 
