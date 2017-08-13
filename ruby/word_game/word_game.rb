156c2d204e1cba048089d111bcedca9ea1e1d43b
Catherine Rice <clr2107@gmail.com>
2017-08-12 22:51:24 -0400

    Update guess method

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,12 +1,21 @@
 class WordGame
-  ##Try setting word = nil or "", store all guesses in hash and print so user can see, if repeated letter print message.
-attr_reader :game_over, :counter
-attr_accessor :user_guess
+
+attr_accessor :user_guess, :all_guesses, :game_over, :counter, :word
 
 def initialize
+  puts "Starting a new game..."
   @game_over = false
   @counter = 0
   @user_guess = []
+  @all_guesses = []
+
+end
+
+def create_user_guess(word)
+word.length.times do |x|
+  @user_guess << " _ "
+end
+puts @user_guess.join("")
 end
 
 #Method to get user input (letter), check if letter is correct/incorrect, and print the current word.
@@ -14,6 +23,7 @@ def guess(word, letter_guess)
   word_array = word.split("")
   word_array.each do |letter_word|
     if letter_word == letter_guess
+      p @all_guesses << letter_guess
       correct_letter = true
       index = word.index(letter_guess)
       @user_guess[index] = letter_guess
@@ -23,6 +33,7 @@ def guess(word, letter_guess)
     end
   end
   if !word.include?(letter_guess)
+    p @all_guesses << letter_guess
     correct_letter = false
     puts "You guessed an incorrect letter! Try again!"
     puts "Your current guess is " + @user_guess.join("")
@@ -36,22 +47,18 @@ def guess(word, letter_guess)
     @game_over = true
     puts "You are out of guesses! Game over."
   end
+
 end
 
-def user_guess(word)
-@user_guess = []
-word.length.times do |x|
-   @user_guess << " _ "
-  end
-end
 end
 
 # DRIVER CODE
 puts "Welcome to the Word Game!"
 game = WordGame.new
 puts "User 1 enters a word."
-puts @user_guess
 word = gets.chomp
+game.create_user_guess(word)
+
 
 while !game.game_over
 puts "User 2 guesses a letter."
