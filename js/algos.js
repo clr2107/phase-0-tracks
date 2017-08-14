9982e180300ab5d663687e7e2156439da7054afb
Catherine Rice <clr2107@gmail.com>
2017-08-14 16:27:14 -0400

    Add create array method

--- js/algos.js
+++ js/algos.js
@@ -13,35 +13,16 @@ longestPhrase(['cat', 'elephant', 'dolphin']);
 longestPhrase(['giraffe', 'dog', 'bird'])
 
 //Release 1: Write a function that takes two objects and checks to see if the objects share at least one key-value pair (return a boolean). Doesn't matter if a property is a string or identifier name.
-function overlap_object(object1, object2) {
-for (var i in object1){
-  if (! object2.hasOwnProperty(i) || object1[i] !== object2[i] ) {
-    return false;
-  }
-}
-return true;
-}
-overlap_object({animal: "cat", sound: 'meow'}, {animal: "cat", sound: 'hiss'})
 
-function checkIfObjectContains(object1, object2) {
-for (var i in object1) {
-    if (object2.hasOwnProperty(i) && object2.hasOwnProperty(object1[i]));
-      console.log(i + "=" + object1[i]);
-      console.log('true');
-      return true;
-  }
+function shareKeyValuePair (object1, object2){
+for (var i in object1)
+if ((Object.keys(object1)[0] == Object.keys(object2)[0]) &&
+object1[i] == object2[i])
+return true;
+else
+return false;
 }
-checkIfObjectContains({animal: "cat", sound: 'hiss'}, {animal: "cat", sound: 'hiss'})
-//
-function checkIfObjectContains2(object1, object2){
-  for (var i in object1){
-    if (object1[i] == object2[i])
-    console.log('true');
-    console.log(i + "=" + object1[i]);
-    return true;
-  }
-}
-checkIfObjectContains2({animal: "cat", sound: 'hiss'}, {animal: "cat", sound: 'hiss'})
+shareKeyValuePair(object1 = {animal: "cat", sound: "meow"}, object2 = {animal: "cat", sound: "meow"})
 
 //Release 2: Write a function that takes an integer for length, and builds and returns an array of strings of the given length.
 //(Argument of 3 returns 3 randomly generated strings of varying length, min of 1 letter and max of 10 letters.)
