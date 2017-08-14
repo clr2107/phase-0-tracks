ab541022ba9bd79ad35ba4a2f467b74ebe1df024
Catherine Rice <clr2107@gmail.com>
2017-08-14 18:06:06 -0400

    Add boolean condition

--- js/javascript-intro-lab/explore.js
+++ js/javascript-intro-lab/explore.js
@@ -1,11 +1,12 @@
 
 //Write a function that takes a string as a parameter and reverses the string.
 //Take the last letter of string and make it the first letter of new string. Take the second to last letter of string and make it the second letter of new string, etc. Add/concatenate the letters together and print to the console.
-
+//Add driver code that prints the variable if some condition is true.
 function reverse(string) {
   var new_string = "";
   for (var i = string.length -1; i >= 0; i --)
   new_string += string[i];
+  if (1==1)
   return new_string;
 }
 reverse("cat")
